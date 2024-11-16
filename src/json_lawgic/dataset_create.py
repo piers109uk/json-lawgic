c71f31f08719c4fa83931434bfda99b48f3bae9f
Piers Concord <piers109uk@gmail.com>
2024-11-16 17:14:51 -0600

    feat: dataset_create file

--- src/json_lawgic/dataset_create.py
+++ src/json_lawgic/dataset_create.py
@@ -2,9 +2,8 @@ from datasets import load_dataset
 
 statutes = load_dataset("data/default", data_files="*.json")
 
-filtered_statutes = statutes.filter(lambda x: not x["repealed"])
-
-print(statutes)
+filtered_statutes = statutes.filter(lambda x: not x["repealed"]).remove_columns(
+    ["repealed"]
+)
 print(filtered_statutes)
-
-# dataset.push_to_hub("piers109uk/MN-Statutes", private=False)
+filtered_statutes.push_to_hub("piers109uk/MN-Statutes", private=False)
