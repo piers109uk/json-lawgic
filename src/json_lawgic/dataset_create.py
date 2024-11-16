8435c208a969f8f444fcf09cc026d0fc1d254cbf
Piers Concord <piers109uk@gmail.com>
2024-11-16 17:12:45 -0600

    feat: code to upload dataset as needed to hf

--- src/json_lawgic/dataset_create.py
+++ src/json_lawgic/dataset_create.py
@@ -1,3 +1,10 @@
 from datasets import load_dataset
 
-dataset = load_dataset("csv", data_files="my_file.csv")
+statutes = load_dataset("data/default", data_files="*.json")
+
+filtered_statutes = statutes.filter(lambda x: not x["repealed"])
+
+print(statutes)
+print(filtered_statutes)
+
+# dataset.push_to_hub("piers109uk/MN-Statutes", private=False)
--- src/json_lawgic/main.py
+++ src/json_lawgic/main.py
@@ -1,5 +1,10 @@
+import json
+
+
 def main():
-    print("Hello from json-lawgic!")
+    with open("data/default/000000001.json") as json_data:
+        d = json.load(json_data)
+        print(d)
 
 
 if __name__ == "__main__":
