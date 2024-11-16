222771bcd8031308fe9e514724876a55fba84bda
Piers Concord <piers109uk@gmail.com>
2024-11-16 16:09:45 -0600

    feat: init python, rename datasets to data

---
+++ src/json_lawgic/dataset_create.py
@@ -0,0 +1,3 @@
+from datasets import load_dataset
+
+dataset = load_dataset("csv", data_files="my_file.csv")
---
+++ src/json_lawgic/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from json-lawgic!")
+
+
+if __name__ == "__main__":
+    main()
