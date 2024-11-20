0614a3a6cd5636415412718bb3ffd7fcf6dc37c0
Piers Concord <piers109uk@gmail.com>
2024-11-20 17:26:58 -0600

    feat: join json, import on web

--- frontend/src/app/statutes.ts
+++ frontend/src/app/statutes.ts
@@ -1,3 +1,5 @@
+import interpretedLaws from "../../public/interpreted-laws.json"
+
 export interface IJsonLogicInterpretation {
   // The pure JSON logic rule expressed as a JSON object
   rule: object
@@ -15,17 +17,4 @@ export interface IStatuteData extends Partial<IJsonLogicInterpretation> {
   text: string | null
 }
 
-export const statutes: IStatuteData[] = [
-  {
-    id: "1.03",
-    url: "https://www.revisor.mn.gov/statutes/cite/1.03",
-    title: "1.03 WATERS INCLUDED.",
-    text: "The concurrent jurisdiction of a county and of courts and officers exercising jurisdiction throughout it extends over the water area that would be included if the boundary lines of the county were produced in the direction of their approach and extended across the waters to the opposite shore.",
-  },
-  {
-    id: "1.01",
-    url: "https://www.revisor.mn.gov/statutes/cite/1.01",
-    title: "1.01 EXTENT.",
-    text: "The sovereignty and jurisdiction of this state extend to all places within its boundaries as defined in the constitution and, concurrently, to the waters forming a common boundary between this and adjoining states, subject only to rights of jurisdiction acquired by the United States over places in it.",
-  },
-]
+export const statutes: IStatuteData[] = interpretedLaws
