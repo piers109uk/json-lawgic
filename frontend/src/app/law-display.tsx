e25408e3a5da87f01a78f288baa5a27b7e27ce02
Piers Concord <piers109uk@gmail.com>
2024-11-20 17:49:58 -0600

    feat: display link to law

--- frontend/src/app/law-display.tsx
+++ frontend/src/app/law-display.tsx
@@ -1,4 +1,5 @@
 import { IStatuteData } from "./statutes"
+import { ArrowTopRightOnSquareIcon } from "@heroicons/react/24/solid"
 
 interface LawDisplayProps {
   selectedLaw: IStatuteData
@@ -7,7 +8,12 @@ interface LawDisplayProps {
 export default function LawDisplay({ selectedLaw }: LawDisplayProps) {
   return (
     <div className="w-2/3 p-4">
-      <h1 className="text-xl font-bold">{selectedLaw.title}</h1>
+      <div className="flex gap-1">
+        <h1 className="text-xl font-bold">{selectedLaw.title}</h1>
+        <a href={selectedLaw.url}>
+          <ArrowTopRightOnSquareIcon className="size-6 text-blue-600 hover:text-blue-800 visited:text-purple-600" />
+        </a>
+      </div>
       <p>{selectedLaw.text}</p>
     </div>
   )
