9f790ccea0e87ff684bf3ba1628fb2f9c2c84c71
Piers Concord <piers109uk@gmail.com>
2024-11-21 17:11:44 -0600

    feat: icon to header, showcase

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,6 +5,8 @@ import JsonLogic from "./json-logic"
 import Search from "./Search"
 import { IStatuteData, statutes } from "./statutes"
 import LawDisplay from "./law-display"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { ScaleIcon } from "@heroicons/react/24/solid"
 
 // TODO: reducer?
 // TODO: Move to provider
@@ -26,18 +28,38 @@ export default function Home() {
   return (
     <>
       <div className="h-screen bg-background">
-        <header className="bg-primary text-primary-foreground p-4">
+        <header className="bg-primary text-primary-foreground p-4 flex gap-1 items-center">
+          <ScaleIcon className="size-6" />
           <h1 className="text-2xl font-bold">Json-Lawgic</h1>
         </header>
-        <div className="flex">
-          <div className="w-1/3 p-4 bg-white shadow-md">
-            <Search statutes={statutes} setSelectedLaw={setSelectedLaw}></Search>
-          </div>
-          <div className="w-2/3 p-4 overflow-auto">
-            <LawDisplay selectedLaw={selectedLaw}></LawDisplay>
-            <JsonLogic data={selectedLaw.examples} rule={selectedLaw.rule} setRule={setRule} setData={setData}></JsonLogic>
-          </div>
-        </div>
+        <Tabs defaultValue="search" className="">
+          <TabsList className="w-full justify-start">
+            <TabsTrigger value="search">Search</TabsTrigger>
+            <TabsTrigger value="showcase">Showcase</TabsTrigger>
+          </TabsList>
+          <TabsContent value="search" className="flex-grow flex">
+            <div className="flex">
+              <div className="w-1/3 p-4 bg-white shadow-md">
+                <Search statutes={statutes} setSelectedLaw={setSelectedLaw}></Search>
+              </div>
+              <div className="w-2/3 p-4 overflow-auto">
+                <LawDisplay selectedLaw={selectedLaw}></LawDisplay>
+                <JsonLogic data={selectedLaw.examples} rule={selectedLaw.rule} setRule={setRule} setData={setData}></JsonLogic>
+              </div>
+            </div>
+          </TabsContent>
+          <TabsContent value="showcase" className="flex-grow flex">
+            <div className="flex">
+              <div className="w-1/3 p-4 bg-white shadow-md">
+                <Search statutes={statutes} setSelectedLaw={setSelectedLaw}></Search>
+              </div>
+              <div className="w-2/3 p-4 overflow-auto">
+                <LawDisplay selectedLaw={selectedLaw}></LawDisplay>
+                <JsonLogic data={selectedLaw.examples} rule={selectedLaw.rule} setRule={setRule} setData={setData}></JsonLogic>
+              </div>
+            </div>
+          </TabsContent>
+        </Tabs>
       </div>
     </>
   )
