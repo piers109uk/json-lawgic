ade6accb2e06095e4c2bbb2d8aefc44e34102db8
Piers Concord <piers109uk@gmail.com>
2024-11-24 09:55:43 -0600

    refactor: support more flexibility in methods, rerun showcase using variable descriptions

---
+++ src/json_lawgic/json_merge.py
@@ -0,0 +1,21 @@
+import json
+import glob
+
+
+def merge_json_files(input_directory: str, output_file: str):
+    """Merges all JSON files in the input directory into a single file."""
+    json_files = glob.glob(f"{input_directory}/*.json")
+    combined_data = []
+    for file in json_files:
+        with open(file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            combined_data.append(data)
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(combined_data, f, indent=2)
+    print(f"Combined JSON written to {output_file}")
+
+
+if __name__ == "__main__":
+    input_directory = "data/examples-interpreted"
+    output_file = "data/showcase.json"
+    merge_json_files(input_directory, output_file)
--- src/json_lawgic/law_interpreter.py
+++ src/json_lawgic/law_interpreter.py
@@ -14,6 +14,8 @@ load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o", temperature=0)
 
+# TODO: enable representing multiple rules with one law
+
 
 class RuleVariable(BaseModel):
     """A variable used in the JSON logic rule."""
